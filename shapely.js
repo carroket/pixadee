692d94536d3ee7a0dca8d51bf27bc9c8d8de8e29
Brian Sexton <brian.sexton@gmail.com>
2015-06-11 01:44:18 -0700

    Add the library itself.

---
+++ shapely.js
@@ -0,0 +1,30 @@
+/*============================================================================*\
+
+	JavaScript for Shapely
+	-----------------------------------------------------------------------
+	© 2015 by Carroket, Inc.
+	http://www.carroket.com/
+	-----------------------------------------------------------------------
+	Made by Brian Sexton.
+	http://www.briansexton.com/
+
+ \*============================================================================*/
+
+
+(function(document, options) {
+
+	var shapely = new Shapely();
+
+	function Shapely() {
+
+		// TO DO: Consider whether any options should be addressed here.
+	}
+
+	// If a namespace was specified, attach shapely to it.
+
+	if (options instanceof Object && options.namespace instanceof Object) {
+
+		options.namespace.shapely = shapely;
+	}
+
+})(window.document);
