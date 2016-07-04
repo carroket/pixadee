df6f53408d94c3f11ef01c9281c1fc8ca04e27ef
Brian Sexton <brian.sexton@gmail.com>
2016-07-03 23:58:28 -0700

    Add clean and copy plugins and tasks for Grunt.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,8 +2,32 @@ module.exports = function (grunt) {
 
 	grunt.initConfig({
 
-		pkg: grunt.file.readJSON('package.json')
+		pkg: grunt.file.readJSON('package.json'),
+
+		clean: {
+			build: {
+				src: "build"
+			}
+		},
+
+		copy: {
+			build: {
+				files: [
+					{
+						expand: true,
+						cwd: 'source/',
+						src: ['pixadee.js'],
+						dest: 'build/'
+					}
+				]
+			}
+		}
 	});
 
-	grunt.registerTask('default', []);
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+
+	grunt.registerTask('build', ['clean', 'copy']);
+
+	grunt.registerTask('default', ['build']);
 };
